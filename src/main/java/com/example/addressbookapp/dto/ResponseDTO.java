2c2497373fba52ed4878b47e26b8cc5341ba09a7
Radhey <radheshyamb9@gmail.com>
2022-05-24 22:10:09 +0530

    ADDED : Introducing lombok library to auto generate getters and setters

--- src/main/java/com/example/addressbookapp/dto/ResponseDTO.java
+++ src/main/java/com/example/addressbookapp/dto/ResponseDTO.java
@@ -1,32 +1,18 @@
 package com.example.addressbookapp.dto;
 
 import com.example.addressbookapp.modal.AddressBookData;
+import lombok.Data;
 
 import java.util.List;
 
-public class ResponseDTO {
-    public String message;
-    public Object data;
+public @Data class ResponseDTO {
+    private String message;
+    private Object data;
 
-    public ResponseDTO(String message,Object data) {
+    public ResponseDTO(String message, Object data) {
         this.message = message;
         this.data = data;
 
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
